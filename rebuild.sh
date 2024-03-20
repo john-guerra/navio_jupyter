de0ad739223ca46afa4cd8f151d3fbcbc660c896
John Alexis Guerra Gómez <john.guerra@gmail.com>
2024-03-20 14:39:18 -0700

    Updating release instructions

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-jupyter nbextension uninstall --py --sys-prefix navio_jupyter
-rm -rf navio_jupyter/static
-python setup.py build
-python install -e .
-pip install -e .
-jupyter nbextension install --py --symlink --sys-prefix navio_jupyter
-jupyter nbextension enable navio_jupyter --py --sys-prefix
+pip install -e ".[dev]"
+npm install
+npm run build
+hatch build
